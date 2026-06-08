553e7e39bc21474364b7f27f82cb75bd421ff70b
Haouabarkallah <haouabarkallah@gmail.com>
2026-06-08 09:38:54 +0000

    Feat: Finalize global state provider and configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-plugin'
+
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/learning_react", // Replace with your actual repo name!
   plugins: [react()],
 })
